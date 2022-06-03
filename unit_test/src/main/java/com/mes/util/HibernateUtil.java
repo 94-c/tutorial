ba2384459dcbe37550c1d2994f3095b7b3dd0bbf
94-c <hyeongwoo26@gmail.com>
2022-06-04 01:32:50 +0900

    servlet_jpa_test 작성 중

---
+++ unit_test/src/main/java/com/mes/util/HibernateUtil.java
@@ -0,0 +1,34 @@
+package com.mes.util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    static {
+        if (entityManagerFactory == null){
+            try{
+                entityManagerFactory = Persistence.createEntityManagerFactory("test");
+            }catch (IllegalStateException e){
+                e.printStackTrace();
+                HibernateUtil.close();
+            }
+        }
+    }
+
+    public static EntityManagerFactory getEntityManagerFactory(){
+        return entityManagerFactory;
+    }
+
+    public static void close(){
+        try {
+            if (entityManagerFactory instanceof  EntityManagerFactory){
+                entityManagerFactory.close();
+            }
+        }catch (IllegalStateException e){
+            System.out.println("DB Connection close Error");
+        }
+    }
+}
